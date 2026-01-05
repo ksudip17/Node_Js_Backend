402c346d8bccc1fa97e9e6000c2f453b71d6956d
Sudip Khatiwada <ksudip121@gmail.com>
2026-01-06 03:02:02 +0530

    Day 2 added open/downloads functionality

--- Google-Drive2.0/app.js
+++ Google-Drive2.0/app.js
@@ -1,19 +1,39 @@
 import { readFile } from "fs/promises";
 import { open, readdir } from "fs/promises";
 import http from "http";
+import mime from "mime-types";
 
 const server = http.createServer(async (req, res) => {
-  if(req.url === "/favicon.ico") return res.end("No Favicon!!")
+  if (req.url === "/favicon.ico") return res.end("No Favicon!!");
   if (req.url === "/") {
-    serveDirectory(req, res);
+    serveDirectory("/", res);
   } else {
     try {
-      const handleFile = await open(`./storage${decodeURIComponent(req.url)}`);
+      const [url, queryString] = req.url.split("?");
+
+      const queryParam = {};
+      if (queryString) {
+        queryString.split("&").forEach((pair) => {
+          const [key, value] = pair.split("=");
+          queryParam[key] = value;
+        });
+      }
+      console.log(queryParam);
+
+      const handleFile = await open(`./storage${decodeURIComponent(url)}`);
       const stats = await handleFile.stat();
+      
       if (stats.isDirectory()) {
-        serveDirectory(req, res);
+        serveDirectory(url, res);
       } else {
         const readStream = handleFile.createReadStream();
+        res.setHeader("Content-Type", mime.contentType(url.slice(1)));
+        res.setHeader("Content-Length", stats.size);
+        
+        if (queryParam.action === "download") {
+          res.setHeader("Content-Disposition", `attachment; filename="${url.slice(1)}"`);
+        }
+        
         readStream.pipe(res);
       }
     } catch (err) {
@@ -23,16 +43,18 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-async function serveDirectory(req, res) {
-  const itemsList = await readdir(`./storage${req.url}`);
-    let dynamicHtml = " ";
-    itemsList.forEach((item) => {
-      dynamicHtml += `<a href=".${
-        req.url === "/" ? "" : req.url
-      }/${item}">${item}</a><br>`;
-    });
-    const htmlBoilerPlate = await readFile("./boilerplate.html", "utf-8");
-    res.end(htmlBoilerPlate.replace("${dynamicHtml}", dynamicHtml));
+async function serveDirectory(url, res) {
+  const itemsList = await readdir(`./storage${url}`);
+  let dynamicHtml = "";
+  itemsList.forEach((item) => {
+    dynamicHtml += `${item} <a href=".${
+      url === "/" ? "" : url
+    }/${item}?action=open">Open</a> <a href=".${
+      url === "/" ? "" : url
+    }/${item}?action=download">Download</a><br>`;
+  });
+  const htmlBoilerPlate = await readFile("./boilerplate.html", "utf-8");
+  res.end(htmlBoilerPlate.replace("${dynamicHtml}", dynamicHtml));
 }
 
 server.listen(8080, "0.0.0.0", () => {
